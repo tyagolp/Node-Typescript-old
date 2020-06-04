704a2ea99bab97062f4d4bbb0929c541918af5ad
TyagoLP <tyagolp@gmail.com>
2020-06-03 23:16:29 -0300

    add model appointment

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  const { name, email } = request.body;
-
-  return response.json({ message: 'Hello World' });
-});
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
